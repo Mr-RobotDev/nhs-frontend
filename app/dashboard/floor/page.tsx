bbbce5d92d155f856e7c7e43a452914a8c01d266
Abdullah Shahid <abdullahshahid1071@gmail.com>
2024-07-30 15:26:08 +0500

    Fixed the tab Name for floor plan

--- app/dashboard/floor/page.tsx
+++ app/dashboard/floor/page.tsx
@@ -1,6 +1,10 @@
 import MainFloorView from '@/components/Dashboard/FloorPlan/MainFloorView'
 import React from 'react'
 
+export const metadata = {
+  title: 'Floor Plan'
+}
+
 const FloorPlanPage = () => {
   return (
     <MainFloorView />
